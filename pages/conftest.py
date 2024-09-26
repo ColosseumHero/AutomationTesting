17eb840ee45bb2a59cdefc267d236c39320dd328
ColosseumHero <zakharchenko.a2002@gmail.com>
2024-09-26 21:34:24 +0300

    Commit 6

--- pages/conftest.py
+++ pages/conftest.py
@@ -1,12 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture
 def driver():
-    # Ініціалізуємо браузер Chrome
-    driver = webdriver.Chrome()
+    # Створюємо об'єкт для налаштувань Chrome
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Безголовий режим
+    chrome_options.add_argument("--no-sandbox")  # Потрібно для серверів CI/CD
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Оптимізація для обмежених ресурсів
+
+    # Ініціалізуємо браузер Chrome з опціями
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10)
+
     yield driver
+
     # Закриваємо браузер після тесту
     driver.quit()
 
