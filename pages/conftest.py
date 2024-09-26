f89b2ae3e71dd93ad10b8384cea1be516301b946
ColosseumHero <zakharchenko.a2002@gmail.com>
2024-09-26 22:12:48 +0300

    Commit 10

--- pages/conftest.py
+++ pages/conftest.py
@@ -1,18 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import pytest
+from selenium import webdriver
 
 @pytest.fixture
 def driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Headless режим
-    chrome_options.add_argument("--no-sandbox")  # Для работы в окружениях CI/CD
-    chrome_options.add_argument("--disable-dev-shm-usage")  # Для минимизации использования shared memory
-    chrome_options.add_argument("--disable-gpu")  # Отключаем GPU, часто помогает при headless тестировании
-    chrome_options.add_argument("--remote-debugging-port=9222")  # Указываем порт для отладки
-
-    driver = webdriver.Chrome(options=chrome_options)
+    # Ініціалізуємо браузер Chrome
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
     yield driver
+    # Закриваємо браузер після тесту
     driver.quit()
 
 
